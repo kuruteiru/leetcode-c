d32e0110b0c2cf681d6009d99573a1bdbaccf48a
MartinKrutil <martin.krutil@email.cz>
2024-03-01 22:42:07 +0100

    0001-two-sum possible solution?

--- problems/0001-two-sum/two-sum.c
+++ problems/0001-two-sum/two-sum.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "two-sum.h"
+
+int* twoSum(int* nums, size_t numsSize, int target, int* returnSize) {
+    for (size_t i = 1; i < numsSize / sizeof(int); i++) {
+        for (size_t j = 0; j < numsSize / sizeof(int); j++) {
+            if (i == j || nums[i] + nums[j] != target) continue;
+
+            int *sumIndices = (int*) malloc(2 * sizeof(int));
+            
+            *sumIndices = i;
+            *(++sumIndices) = j;
+            
+            return &sumIndices;
+        }
+    }
+    
+    return NULL;
+}
+
+void solve() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+
+    int *sum = twoSum(nums, sizeof(nums), target, 2);
+
+    printf("[%d, %d]", *sum, *(++sum));
+}
--- problems/0001-two-sum/two-sum.h
+++ problems/0001-two-sum/two-sum.h
@@ -1,4 +1,8 @@
 #ifndef TWO_SUM_H
 #define TWO_SUM_H
 
+int* twoSum(int *nums, size_t numsSize, int target, int *returnSize);
+
+void solve();
+
 #endif //TWO_SUM_H
