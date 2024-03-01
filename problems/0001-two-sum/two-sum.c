19a12bbf786581b6b7acd60f5d6a3e9a514001c2
MartinKrutil <martin.krutil@email.cz>
2024-03-01 23:29:55 +0100

    0001-two-sum brute force  solution

--- problems/0001-two-sum/two-sum.c
+++ problems/0001-two-sum/two-sum.c
@@ -4,27 +4,34 @@
 #include "two-sum.h"
 
 int* twoSum(int* nums, size_t numsSize, int target, int* returnSize) {
-    for (size_t i = 1; i < numsSize / sizeof(int); i++) {
-        for (size_t j = 0; j < numsSize / sizeof(int); j++) {
-            if (i == j || nums[i] + nums[j] != target) continue;
+    for (size_t i = 1; i < numsSize; i++) {
+        for (size_t j = 0; j < numsSize; j++) {
+            if (i == j || nums[i] + nums[j] != target) 
+                continue;
 
             int *sumIndices = (int*) malloc(2 * sizeof(int));
             
-            *sumIndices = i;
-            *(++sumIndices) = j;
-            
-            return &sumIndices;
+            sumIndices[0] = i;
+            sumIndices[1] = j;
+
+            *returnSize = 2;
+            return sumIndices;
         }
     }
     
+    *returnSize = 0;
     return NULL;
 }
 
 void solve() {
     int nums[] = {2, 7, 11, 15};
     int target = 9;
+    int returnSize = 0;
 
-    int *sum = twoSum(nums, sizeof(nums), target, 2);
+    int *sum = twoSum(nums, sizeof(nums), target, &returnSize);
 
-    printf("[%d, %d]", *sum, *(++sum));
+    if (returnSize == 2)
+        printf("[%d, %d]", *sum, *(sum + 1));
+    else
+        printf("[]");
 }
